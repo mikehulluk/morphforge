084a89a54e03c09ff2e728a14f63a78a057de972
Mike Hull <mikehulluk@googlemail.com>
2012-04-02 20:43:52 +0100

    Add morphology examples

---
+++ src/morphforgeexamples/morphology/morphology30.py
@@ -0,0 +1,36 @@
+#-------------------------------------------------------------------------------
+# Copyright (c) 2012 Michael Hull.
+# All rights reserved.
+# 
+# Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+# 
+#  - Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+#  - Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+# 
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+#-------------------------------------------------------------------------------
+
+
+
+"""Load  SWC data from a string directly into a MorphologyArray.
+We can load .swc from any file-like object, so we can use StringIO to load directly from strings.
+"""
+
+
+from morphforge.core.misc import StripCommentsAndBlankLines
+from morphforge.morphology.core.morphologyarray import MorphologyArray
+from StringIO import StringIO
+
+swcSrc = """
+1 0 1.0 2.0 3.0 4.0 -1
+2 0 5.0 6.0 7.0 8.0 1
+"""
+m = MorphologyArray.fromSWC(StringIO(swcSrc))
+
+print 'Morphology Vertices:'
+print m._vertices
+
+print 'Morphology Connectivity:'
+print m._connectivity
+
+        
---
+++ src/morphforgeexamples/morphology/morphology40.py
@@ -0,0 +1,45 @@
+#-------------------------------------------------------------------------------
+# Copyright (c) 2012 Michael Hull.
+# All rights reserved.
+# 
+# Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+# 
+#  - Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+#  - Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+# 
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+#-------------------------------------------------------------------------------
+
+"""
+Converting between the Morphology representations
+  
+"""
+
+from morphforge.core import LocMgr, Join 
+from morphforge.morphology.core import MorphologyTree
+
+
+testSrcsPath = LocMgr().getTestSrcsPath()
+srcSWCFile = Join(testSrcsPath, "swc_srcs/28o_spindle20aFI.CNG.swc")
+
+mTree = MorphologyTree.fromSWC(src=open(srcSWCFile))
+mArray = mTree.to_array()
+
+print 'Vertex Data'
+print mArray._vertices
+
+
+# Convert back
+mTree2 = mArray.to_tree()
+
+import difflib
+diff = difflib.unified_diff(
+                     mTree.toSWCStr(),
+                     mTree2.toSWCStr()
+                     )
+assert mTree.toSWCStr() == mTree.toSWCStr()
+print 'Finished OK'
+
+
+
+
