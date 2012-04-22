c595e770af00b0c39e34270ebcab6cda33fd0ddb
Michael Hull <mikehulluk@googlemail.com>
2012-04-22 11:43:20 +0100

    Added NeuroUnits -> morphforge bridge

---
+++ src/morphforgecontrib/simulation/membranemechanisms/neurounits/neuro_units_bridge.py
@@ -0,0 +1,165 @@
+#-------------------------------------------------------------------------------
+# Copyright (c) 2012 Michael Hull.
+# All rights reserved.
+# 
+# Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+# 
+#  - Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+#  - Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+# 
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+#-------------------------------------------------------------------------------
+
+
+from morphforge.simulation.neuron.biophysics.mm_neuron import MM_Neuron_Base
+from morphforge.simulation.neuron.neuronsimulationenvironment import NeuronSimulationEnvironment
+from morphforge.simulation.core.biophysics.membranemechanism import MembraneMechanism
+from neurounits.tools.nmodl import WriteToNMODL, MechanismType
+#from morphforge.simulation.neuron.simulationdatacontainers.mhocfile import MHocFileData,\
+#    MHOCSections
+from morphforge.simulation.neuron.biophysics.modfile import ModFile
+#import Cheetah
+from morphforge.simulation.neuron.objects.neuronrecordable import NeuronRecordableOnLocation
+from morphforge.simulation.neuron.hocmodbuilders.hocmodutils import HocModUtils
+from morphforgecontrib.simulation.membranemechanisms.common.neuron import build_HOC_default
+from neurounits.neurounitparser import NeuroUnitParser
+
+
+
+class RecordableData(object):
+    def __init__(self, standard_tags=None):
+        self.standard_tags = standard_tags or []
+        
+
+
+class MM_Neuron_RecGen(NeuronRecordableOnLocation):
+    def __init__(self, srcChl, modvar,unit_in_nrn, std_tags, **kwargs):
+        super( MM_Neuron_RecGen, self).__init__(**kwargs)
+        self.srcChl = srcChl
+        self.modvar=modvar
+        self.unit_in_nrn = unit_in_nrn
+        self.std_tags = std_tags or []
+
+    def buildMOD(self, modFileSet):
+        pass   
+ 
+    def buildHOC(self, hocFile):
+        HocModUtils.CreateRecordFromModFile( hocFile, 
+                                             vecname="RecVec%s"%self.name, 
+                                             celllocation=self.where, 
+                                             modvariable=self.modvar, 
+                                             #mod_neuronsuffix=self.srcChl.getNeuronSuffix(), 
+                                             mod_neuronsuffix=self.srcChl.NRNSUFFIX,
+                                             
+                                             recordobj=self)
+
+    def getDescription(self):
+        return "%s %s %s" % (self.modvar, self.srcChl.name, self.where.getLocationDescriptionStr() ) 
+
+    def getUnit(self):
+        return self.unit_in_nrn
+    def getStdTags(self):
+        return self.std_tags
+
+
+
+
+
+
+class NeuroUnitEqnsetMechanism(MembraneMechanism):
+    def __init__(self, name, eqnset ,mechanism_id, default_parameters={}, recordables_map= None, recordables_data=None):    
+        MembraneMechanism.__init__(self, mechanism_id=mechanism_id)
+        
+        if isinstance( eqnset, basestring):
+            eqnset = NeuroUnitParser.EqnSet(eqnset)
+        
+        self.name = name
+        self._parameters = default_parameters
+        self.eqnset = eqnset
+        self.recordables_map = recordables_map or {}
+        self.recordables_data =recordables_data or {}
+        
+        
+        
+        for param in eqnset.parameters:
+            print param
+            print param.symbol
+            print 'iii', param.get_dimension().as_quantities_unit(), type(param.get_dimension().as_quantities_unit())
+            print "iiii",default_parameters[param.symbol], type( default_parameters[param.symbol])
+            assert param.symbol in default_parameters
+            assert (param.get_dimension().as_quantities_unit() / default_parameters[param.symbol] ).rescale("")
+
+    def getDefaults(self):
+        return self._parameters
+    
+    def getVariables(self):
+        return self._parameters.keys()
+
+
+
+class Neuron_NeuroUnitEqnsetMechanism( MM_Neuron_Base, NeuroUnitEqnsetMechanism):
+    def __init__(self, **kwargs):
+        MM_Neuron_Base.__init__(self)
+        NeuroUnitEqnsetMechanism.__init__(self, **kwargs)
+
+        self.nmodl_txt, self.buildparameters = WriteToNMODL(self.eqnset)
+        
+        
+
+        
+        assert self.buildparameters.mechanismtype == MechanismType.Distributed    
+        self.units = {}
+        for param_str, value in self._parameters.iteritems():
+            sym = self.eqnset.get_terminal_obj(param_str)
+            param_default_unit = self.buildparameters.symbol_units[sym]
+            self.units[param_str] = param_default_unit.as_quantities_unit()
+    
+        self.NRNSUFFIX = self.buildparameters.suffix
+    
+    def build_HOC_Section( self, cell, section, hocFile, mta ):
+        build_HOC_default( cell=cell, section=section, hocFile=hocFile, mta=mta , units=self.units, nrnsuffix=self.buildparameters.suffix )
+    
+    def createModFile(self, modFileSet):  
+        modFileSet.append(ModFile(name=self.name, modtxt=self.nmodl_txt ))
+
+
+    def getModFileChangeables(self):
+        change_attrs = set(['name',"nmodl_txt", 'mechanism_id',  'recordables_map', 'buildparameters', 'units', 'recordables_data'])
+        fixed_attrs = set( ['mm_neuronNumber','cachedNeuronSuffix','eqnset','_parameters',] )
+        print set( self.__dict__)
+        assert set( self.__dict__) == fixed_attrs | change_attrs       
+        return dict ( [ (a, getattr(self, a)) for a in change_attrs ] )
+    
+
+
+
+    def getRecordables(self):    
+        return  self._getRecordableSymbols()
+        assert False    
+        
+
+    def _getRecordableSymbols(self):
+        return [ s.symbol for s in list(self.eqnset.states) + list(self.eqnset.assignedvalues) + list(self.eqnset.suppliedvalues) + list(self.eqnset.parameters) ]
+
+    def getRecordable(self, what, celllocation, **kwargs):
+        
+        # Map it through the recordables_map, so that we can alias to StandardTags:
+        what = self.recordables_map.get(what,what)
+        
+        valid_symbols = self._getRecordableSymbols() #[ s.symbol for s in self.eqnset.states + self.eqnset.assignedvalues + self.eqnset.suppliedvalues + self.eqnset.parameters]
+        if not what in  valid_symbols:
+            err ="Unknown record value: %s. Expecting one of: %s "%(what, valid_symbols)
+            raise ValueError(err)
+        
+        obj = self.eqnset.get_terminal_obj( what )
+        unit_in_nrn = self.buildparameters.symbol_units[obj].as_quantities_unit()
+        
+        std_tags = []
+        if what in self.recordables_data:
+            std_tags = self.recordables_data[what].standard_tags
+        
+        return MM_Neuron_RecGen( srcChl=self, modvar=what, where=celllocation, unit_in_nrn=unit_in_nrn, std_tags=std_tags, **kwargs)
+       
+
+
+NeuronSimulationEnvironment.membranemechanisms.registerPlugin( NeuroUnitEqnsetMechanism, Neuron_NeuroUnitEqnsetMechanism)
---
+++ src/morphforgecontrib/simulation/membranemechanisms/neurounits/test1.py
@@ -0,0 +1,194 @@
+#-------------------------------------------------------------------------------
+# Copyright (c) 2012 Michael Hull.
+# All rights reserved.
+# 
+# Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+# 
+#  - Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+#  - Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+# 
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+#-------------------------------------------------------------------------------
+from neurounits.units_wrapper import NeuroUnitParser
+from morphforgecontrib.simulation.indev.neuro_units_bridge.neuro_units_bridge import NeuroUnitEqnsetMechanism,\
+    RecordableData
+
+
+textHH = r"""EQNSET chlstd_hh_k {
+    from std.math import exp
+    i = g * (v-erev) * n**4
+
+    ninf = n_alpha_rate / (n_alpha_rate + n_beta_rate)
+    ntau = 1.0 / (n_alpha_rate + n_beta_rate)
+    n' = (ninf-n) / ntau
+    StdFormAB(V,a1,a2,a3,a4,a5) = (a1 + a2*V)/(a3+exp( (V+a4)/a5) )  
+    n_alpha_rate = StdFormAB( V=v,a1=n_a1,a2=n_a2,a3=n_a3,a4=n_a4,a5=n_a5)
+    n_beta_rate =  StdFormAB( V=v,a1=n_b1,a2=n_b2,a3=n_b3,a4=n_b4,a5=n_b5)
+    <=> PARAMETER g
+    <=> PARAMETER erev
+    <=> PARAMETER n_a1, n_a2, n_a3, n_a4, n_a5
+    <=> PARAMETER n_b1, n_b2, n_b3, n_b4, n_b5
+    <=> OUTPUT    i:(mA/cm2)      METADATA {"mf":{"role":"TRANSMEMBRANECURRENT"} }
+    <=> INPUT     v: mV           METADATA {"mf":{"role":"MEMBRANEVOLTAGE"} }
+}"""
+
+
+
+textHH = r"""EQNSET chlstd_hh_k {
+    from std.math import exp
+    mult = m**3*h
+    g = gmax* mult
+    i =  g * (v-erev) 
+    minf = m_alpha_rate / (m_alpha_rate + m_beta_rate)
+    mtau = 1.0 / (m_alpha_rate + m_beta_rate)
+    m' = (minf-m) / mtau
+    hinf = h_alpha_rate / (h_alpha_rate + h_beta_rate)
+    htau = 1.0 / (h_alpha_rate + h_beta_rate)
+    h' = (hinf-h) / htau
+    StdFormAB(V,a1,a2,a3,a4,a5) = (a1 + a2*V)/(a3+exp( (V+a4)/a5) )
+    m_alpha_rate = StdFormAB( V=v,a1=m_a1,a2=m_a2,a3=m_a3,a4=m_a4,a5=m_a5)
+    m_beta_rate =  StdFormAB( V=v,a1=m_b1,a2=m_b2,a3=m_b3,a4=m_b4,a5=m_b5)
+    h_alpha_rate = StdFormAB( V=v,a1=h_a1,a2=h_a2,a3=h_a3,a4=h_a4,a5=h_a5)
+    h_beta_rate =  StdFormAB( V=v,a1=h_b1,a2=h_b2,a3=h_b3,a4=h_b4,a5=h_b5)
+    <=> PARAMETER gmax
+    <=> PARAMETER erev
+    <=> PARAMETER m_a1, m_a2, m_a3, m_a4, m_a5
+    <=> PARAMETER m_b1, m_b2, m_b3, m_b4, m_b5
+    <=> PARAMETER h_a1, h_a2, h_a3, h_a4, h_a5
+    <=> PARAMETER h_b1, h_b2, h_b3, h_b4, h_b5
+    <=> OUTPUT    i:(mA/cm2)      METADATA {"mf":{"role":"TRANSMEMBRANECURRENT"} }
+    <=> INPUT     v: mV                 METADATA {"mf":{"role":"MEMBRANEVOLTAGE"} }
+}"""
+
+
+
+
+
+
+
+
+
+from morphforge.stdimports import *
+from morphforgecontrib.stdimports import *
+
+#NeuroUnitEqnsetMechanism
+
+# Create the morphology for the cell:
+morphDict1 = {'root': {'length': 20, 'diam': 20, 'id':'soma'} }
+m1 = MorphologyLoader.fromDictionary(morphDict1)
+
+# Create the environment:
+env = NeuronSimulationEnvironment()
+
+# Create the simulation:
+mySim = env.Simulation(name="TestSim1")
+
+
+# Create a cell:
+myCell = mySim.createCell(name="Cell1", morphology=m1)
+
+
+# Apply the mechanisms to the cells
+leakChannels = env.MembraneMechanism( EqnSetChl, 
+                                      mechanism_id='ID1',
+                                      eqnset=EquationSetLoader.load( 'std_leak_chl.txt', dir= LocMgr.getTestEqnSetsPath() ) , 
+                                      parameters= {'gl':unit("5:pS/um2"), 'eRev': unit("-70:mV")} )
+shortcuts.ApplyMechanismEverywhereUniform(myCell, leakChannels )
+shortcuts.ApplyPassiveEverywhereUniform(myCell, PassiveProperty.SpecificCapacitance, unit('1.0:uF/cm2') )
+
+        
+
+
+hhChannel_params = {
+        'm_a1': unit('13.01e3:s-1'),
+        'm_a2': unit('0e0:/V s'),
+        'm_a3': unit('4.0:'),
+        'm_a4': unit('6.01e-3:V'),
+        'm_a5': unit('-12.56e-3:V'),
+    
+        'm_b1': unit('5.73e3:s-1'),
+        'm_b2': unit('0e3:/V s'),
+        'm_b3': unit('1.0:'),
+        'm_b4': unit('16.01e-3:V'),
+        'm_b5': unit('9.69e-3:V'),
+    
+        'h_a1': unit('0.04e3:s-1'),
+        'h_a2': unit('0.0e3:/V s'),
+        'h_a3': unit('1.0:'),
+        'h_a4': unit('29.88e-3:V'),
+        'h_a5': unit('26e-3:V'),
+    
+        'h_b1': unit('2.04e3:s-1'),
+        'h_b2': unit('0.0e3:/V s'),
+        'h_b3': unit('1:'),
+        'h_b4': unit('-8.09e-3:V'),
+        'h_b5': unit('-10.21e-3:V'),
+        'gmax':    unit("120:pS/um2"), 
+        'erev': unit("50:mV"),}
+
+
+
+
+
+hhChannels = env.MembraneMechanism( NeuroUnitEqnsetMechanism, 
+                                      name = "bklk",
+                                      mechanism_id='ID2',
+                                      eqnset=NeuroUnitParser.File(text=textHH).get_eqnset('chlstd_hh_k'), 
+                                      default_parameters= hhChannel_params,
+                                      recordables_map= {},
+                                      recordables_data = {
+                                                          "i": RecordableData(standard_tags=[StandardTags.CurrentDensity])
+                                                          } 
+                                      
+                                      )
+shortcuts.ApplyMechanismEverywhereUniform(myCell, hhChannels )
+
+
+
+
+
+
+
+
+# Get a location on the cell:
+somaLoc = myCell.getLocation("soma")
+
+# Create the simulous:
+mySim.createCurrentClamp( name="Stim1", amp=unit("250:pA"), dur=unit("100:ms"), delay=unit("100:ms"), celllocation=somaLoc)
+
+
+# Define what to record:
+mySim.record( myCell, what=StdRec.MembraneVoltage, name="SomaVoltage", location = somaLoc, description='Membrane Voltage')
+#mySim.recordall( leakChannels, where=somaLoc)
+
+mySim.record(hhChannels, what="i", celllocation=somaLoc )
+mySim.record(hhChannels, what="g", celllocation=somaLoc, user_tags=[StandardTags.ConductanceDensity])
+mySim.record(hhChannels, what="m", celllocation=somaLoc, user_tags=[StandardTags.StateVariable])
+mySim.record(hhChannels, what="mult", celllocation=somaLoc, user_tags=[StandardTags.StateVariable])
+mySim.record(hhChannels, what="h", celllocation=somaLoc, user_tags=[StandardTags.StateVariable])
+
+# Run the simulation
+results = mySim.Run()
+
+
+
+
+
+ps = (   
+                PlotSpec_DefaultNew( s="Voltage", ylabel='Voltage', yrange=(-60*mV,40*mV)  ),
+                PlotSpec_DefaultNew( s="CurrentDensity", ylabel='CurrentDensity', yunit=unit("pA/um2")  ),
+                PlotSpec_DefaultNew( s="Current", ylabel='Current',yunit=pq.picoamp ), 
+                PlotSpec_DefaultNew( s="Conductance", ylabel="Conductance" ),
+                PlotSpec_DefaultNew( s="ConductanceDensity", ylabel="ConductanceDensity", yunit=unit("pS/um2")  ),
+                PlotSpec_DefaultNew( s="StateVariable", ylabel="StateVariable" ), 
+                PlotSpec_DefaultNew( s="StateTimeConstant",yunit=pq.millisecond, ylabel="Time Constant"  ),
+                PlotSpec_DefaultNew( s="StateSteadyState", ylabel="Steady State" ),                 
+                PlotSpec_DefaultNew( s="Event", ylabel="Events" ),
+
+                )
+
+# Display the results:
+TagViewer([results], timeranges=[(95, 200)*pq.ms], plotspecs = ps )
+pylab.show()
+
+
