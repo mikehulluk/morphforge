179f77d378989cba1da72a66778dc7b82a5d9ed8
Michael Hull <mikehulluk@googlemail.com>
2012-06-23 17:13:32 +0100

    Made all headers uniform

--- src/morphforgecontrib/mhdev/simulation/membranemechanisms/hh_style/mmcalciumalphabetabeta/mmwriter_caalphabetabeta.py
+++ src/morphforgecontrib/mhdev/simulation/membranemechanisms/hh_style/mmcalciumalphabetabeta/mmwriter_caalphabetabeta.py
@@ -1,26 +1,33 @@
-#-------------------------------------------------------------------------------
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# ---------------------------------------------------------------------
 # Copyright (c) 2012 Michael Hull.
 # All rights reserved.
 #
-# Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
 #
-#  - Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
-#  - Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+#  - Redistributions of source code must retain the above copyright 
+#    notice, this list of conditions and the following disclaimer. 
+#  - Redistributions in binary form must reproduce the above copyright 
+#    notice, this list of conditions and the following disclaimer in 
+#    the documentation and/or other materials provided with the 
+#    distribution.
 #
-# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-#-------------------------------------------------------------------------------
-#        self.permeability = unit(permeability)
-#        self.intracellular_concentration  =unit( intracellular_concentration )
-#        self.extracellular_concentration = unit( extracellular_concentration )
-#
-#        self.eqn = equation
-#        self.statevars = dict([ (s, (sDict['alpha'], sDict['beta1'], sDict['beta2'])) for s, sDict in statevars.iteritems()])
-#        self.beta2threshold = unit(beta2threshold)
-#
-#        self.F = unit("96480:C/mol")
-#        self.R = unit("8.314472:J/K/mol")
-#        #self T = unit("300.0:K")
-#        self.T = unit(temperature)
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS 
+# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT 
+# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR 
+# A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT 
+# HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+# SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT 
+# LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+# DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY 
+# THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT 
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+#  OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+# ----------------------------------------------------------------------
 
 
 
--- src/morphforgeexamples/assorted/util.py
+++ src/morphforgeexamples/assorted/util.py
@@ -1,48 +1,32 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# ---------------------------------------------------------------------
+# Copyright (c) 2012 Michael Hull.
+# All rights reserved.
 #
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
 #
+#  - Redistributions of source code must retain the above copyright 
+#    notice, this list of conditions and the following disclaimer. 
+#  - Redistributions in binary form must reproduce the above copyright 
+#    notice, this list of conditions and the following disclaimer in 
+#    the documentation and/or other materials provided with the 
+#    distribution.
 #
-#
-#import glob
-#
-#from os.path import join as Join
-#from neurounits.importers.neuroml import ChannelMLReader, NeuroMLFileContainsNoChannels #parse_channelml_file
-#
-#
-#
-#
-#subdirs = [
-#    "CA1PyramidalCell_NeuroML",
-#    "GranCellLayer_NeuroML",
-#    "GranuleCell_NeuroML",
-#    "MainenEtAl_PyramidalCell_NeuroML",
-#    "SolinasEtAl_GolgiCell_NeuroML",
-#    "Thalamocortical_NeuroML",
-#    "VervaekeEtAl-GolgiCellNetwork_NeuroML",
-#]
-#
-#simSrcDir = "/home/michael/hw_to_come/morphforge/src/test_data/NeuroML/V1/example_simulations/"
-#
-#
-#
-#
-#
-#
-#def get_chl_info_dir():
-#    chlInfo = []
-#
-#
-#
-#    for subdir in subdirs:
-#
-#        for xmlfile in glob.glob( Join(simSrcDir, subdir) + '/*.xml'):
-#
-#            try:
-#                ChannelMLReader.LoadChlRaw(xmlfile)
-#                chlInfo.append(  xmlfile )
-#            except NeuroMLFileContainsNoChannels,e:
-#                pass
-#            except:
-#                raise
-#
-#
-#    return chlInfo
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS 
+# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT 
+# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR 
+# A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT 
+# HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+# SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT 
+# LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+# DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY 
+# THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT 
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+#  OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+# ----------------------------------------------------------------------
+
+
