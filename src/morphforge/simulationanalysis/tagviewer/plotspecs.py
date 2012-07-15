cfa4c315132beb486086796315f77a364bba165d
Michael Hull <mikehulluk@googlemail.com>
2012-07-15 12:18:39 +0100

    More refactoring (tagviewer)

---
+++ src/morphforge/simulationanalysis/tagviewer/plotspecs.py
@@ -0,0 +1,235 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# ---------------------------------------------------------------------
+# Copyright (c) 2012 Michael Hull.
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+#
+#  - Redistributions of source code must retain the above copyright 
+#    notice, this list of conditions and the following disclaimer. 
+#  - Redistributions in binary form must reproduce the above copyright 
+#    notice, this list of conditions and the following disclaimer in 
+#    the documentation and/or other materials provided with the 
+#    distribution.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS 
+# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT 
+# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR 
+# A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT 
+# HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+# SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT 
+# LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+# DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY 
+# THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT 
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+#  OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+# ----------------------------------------------------------------------
+
+import numpy as np
+from morphforge.core.quantities.fromcore import unit
+import quantities as pq
+
+from morphforge.traces import TagSelector
+
+
+def default_legend_labeller(tr):
+    if tr.comment:
+        return tr.comment
+    elif tr.name:
+        return tr.name
+    else:
+        return None
+
+
+
+
+
+
+class YAxisConfig(object):
+    def __init__(self, yunit=None, yrange=None, ylabel=None, yticks=None, yticklabels=None, ymargin=None, ynticks=None):
+        self.yrange = yrange
+        self.yunit = yunit
+        self.ylabel = ylabel
+        self.ynticks= ynticks if ynticks is not None else 5
+        pass
+
+    def format_axes(self, ax):
+        ax.set_ylabel( self.ylabel )
+        if self.yrange is not None:
+            ax.set_ylim( self.yrange )
+        if self.yunit is not None:
+            ax.set_display_unit(y=self.yunit)
+
+        #ax.xaxis.set_major_locator( mpl.ticker.MaxNLocator(4) )
+        ax.set_yaxis_maxnlocator(self.ynticks)
+
+
+class PlotSpec_DefaultNew( object):
+
+    def __init__(self, s, title=None, legend_labeller=default_legend_labeller, colors=None, event_marker_size=None, time_range=None, ylabel=None, yrange=None, yunit=None, ynticks=None, yaxisconfig=None):
+        #self.yrange = yrange
+        #self.yunit = yunit
+        #self.ylabel = ylabel if ylabel else s
+
+        if yaxisconfig is None: 
+            self.yaxisconfig=YAxisConfig(ylabel=ylabel if ylabel else s, 
+                                         yunit=yunit, 
+                                         yrange=yrange,
+                                         ynticks=ynticks)
+        else:
+            self.yaxisconfig = yaxisconfig
+
+
+
+        self.title = title
+        self.legend_labeller = legend_labeller
+        self.colors = colors
+
+        self.event_marker_size = event_marker_size
+        self.time_range = time_range
+
+
+        if isinstance(s, TagSelector):
+            self.selector = s
+        elif isinstance(s, basestring):
+            self.selector = TagSelector.from_string(s)
+        else:
+            assert False
+
+
+    #def _get_selector_ylabel(self):
+    #    return self.ylabel
+
+    # Used by TagViewer
+    def addtrace_predicate(self, trace):
+        return self.selector(trace)
+    def addeventset_predicate(self, trace):
+        return self.selector(trace)
+
+
+    # Plot in order by name; this is normally fine, since annonymous objects
+    # will be plotted in the order they were created.
+    def _sort_traces(self, traces):
+        return sorted( traces, key=lambda t : t.name)
+    def _sort_eventsets(self, event_sets):
+        return sorted( event_sets, key=lambda t : t.name)
+
+
+    def _plot_trace(self, trace,  ax, index, color=None):
+        plot_kwargs = {}
+
+
+        if self.legend_labeller is not None:
+            plot_kwargs['label'] = self.legend_labeller(trace)
+
+
+        if color is not None:
+            plot_kwargs['color'] = color
+        else:
+            if self.colors:
+                plot_kwargs['color'] = self.colors[ index % len(self.colors) ]
+
+        plt_tr =  ax.plotTrace(trace, **plot_kwargs)
+        return plt_tr
+
+    def _plot_eventset(self, eventset, ax, index):
+        if len(eventset) == 0:
+            return []
+
+
+        plot_kwargs = {}
+        if self.event_marker_size:
+            plot_kwargs['markersize'] = self.event_marker_size
+
+        if self.legend_labeller is not None:
+            plot_kwargs['label'] = self.legend_labeller(eventset)
+
+
+        if 'label' in plot_kwargs:
+            assert isinstance(plot_kwargs['label'], basestring)
+
+        i_range = 0.2
+        i_scale = i_range / len( list(eventset.times) )
+
+        data = np.array( [ (t.rescale("ms").magnitude,index+i*i_scale) for i,t in enumerate(eventset.times) ] )
+
+
+
+        from morphforge.stdimports import pq
+        p= ax.plot( data[:,0] * pq.ms, data[:,1] * pq.dimensionless ,'o',ms=2, **plot_kwargs )
+        return p
+
+
+
+
+    def plot(self, ax, all_traces,  all_eventsets, plot_xaxis_details, time_range=None, linkage=None, ) :
+        if self.time_range is not None:
+            time_range = self.time_range
+
+        # Which traces are we plotting (rely on a mixon class):
+        trcs = [tr for tr in all_traces if self.addtrace_predicate(tr)]
+        eventsets = [tr for tr in  all_eventsets if self.addeventset_predicate(tr)]
+
+
+        # Sort and plot:
+        for index, trace in enumerate( self._sort_traces(trcs) ):
+            color = linkage.color_allocations.get(trace, None) if linkage else None
+            self._plot_trace( trace, ax=ax, index=index, color=color)
+
+
+        for index, event_set in enumerate( self._sort_eventsets(eventsets) ):
+            self._plot_eventset( event_set,  ax=ax, index=index+len(trcs) )
+
+            #ax.set_ylim( ( (-0.5) * pq.dimensionless, (len(eventsets)+0.5) * pq.dimensionless ) )
+
+
+        if len(trcs) == 0:
+            padding =0.5
+            ax.set_yunit( 1*pq.dimensionless )
+            ax.set_ylim( ( (-padding) * pq.dimensionless, (len(eventsets)-1+padding) * pq.dimensionless ) )
+
+        #Legend:
+        if self.legend_labeller is not None:
+            import math
+            import __builtin__ as BI
+            ncols = BI.max( int( math.floor( len(trcs) / 5.0) ), 1)
+            ax.legend(ncol=ncols)
+
+        if self.title:
+            ax.set_title( self.title )
+
+        # Label up the axis:
+        if plot_xaxis_details:
+            ax.set_xlabel('Time')
+        else:
+            ax.set_xlabel('')
+            ax.set_xticklabels([])
+
+        #ax.set_xunit( unit('ms') )
+        #print ax.xyUnitBase[0]
+        #print ax.xyUnitDisplay[0]
+        #assert False
+
+
+        # Setup the y-axis:
+        #ax.set_ylabel( self.yaxisconfig.ylabel )
+        self.yaxisconfig.format_axes(ax)
+
+
+
+        if time_range is not None:
+            #print 'Setting Time Range', time_range
+            ax.set_xlim( time_range )
+        #if self.yaxisconfig.yrange is not None:
+        #    ax.set_ylim( self.yaxisconfig.yrange )
+
+        #if self.yunit is not None:
+        #    #print 'Setting Yunit', self.yaxisconfig.yunit
+        #    ax.set_display_unit(y=self.yaxisconfig.yunit)
+
+        # Turn the grid on:
+        ax.grid('on')
---
+++ src/morphforge/simulationanalysis/tagviewer/tagviewer.py
@@ -0,0 +1,231 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# ---------------------------------------------------------------------
+# Copyright (c) 2012 Michael Hull.
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+#
+#  - Redistributions of source code must retain the above copyright 
+#    notice, this list of conditions and the following disclaimer. 
+#  - Redistributions in binary form must reproduce the above copyright 
+#    notice, this list of conditions and the following disclaimer in 
+#    the documentation and/or other materials provided with the 
+#    distribution.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS 
+# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT 
+# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR 
+# A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT 
+# HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+# SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT 
+# LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+# DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY 
+# THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT 
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+#  OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+# ----------------------------------------------------------------------
+
+from morphforge.core import is_iterable
+from morphforge.core import unit
+from morphforge.simulation.base import SimulationResult
+from morphforge.core.quantities import mV, ms, Quantity
+from mhlibs.quantities_plot import QuantitiesFigure
+from plotspecs import PlotSpec_DefaultNew
+from morphforge.traces import  TraceFixedDT, TraceVariableDT, TracePiecewise
+from morphforge.traces.eventset import EventSet
+from morphforge.core import quantities as pq
+from mreorg.scriptplots import PM
+
+
+
+
+
+def _resolve_time_range(time_range):
+    # Sort out the time_range:
+    if time_range is not None:
+        if  isinstance(time_range, (tuple, list, Quantity) ):
+            if len(time_range) == 2:
+                if isinstance(time_range[0], Quantity ):
+                    pass
+                else:
+                    assert False
+                    time_range = time_range * ms
+        else:
+            assert False
+    return time_range
+
+
+
+
+class DefaultPlotSpec(object):
+    Voltage =            PlotSpec_DefaultNew( s="Voltage", ylabel='Voltage', yrange=(-80*mV,50*mV), yunit=pq.millivolt  )
+    CurrentDensity =     PlotSpec_DefaultNew( s="CurrentDensity", ylabel='CurrentDensity', yunit=pq.milliamp/pq.cm2  )
+    Current =            PlotSpec_DefaultNew( s="Current", ylabel='Current',yunit=pq.picoamp )
+    Conductance =        PlotSpec_DefaultNew( s="Conductance", ylabel="Conductance" )
+    ConductanceDensity = PlotSpec_DefaultNew( s="ConductanceDensity", ylabel="ConductanceDensity", yunit=pq.milli * pq.siemens / pq.cm2  )
+    StateVariable =      PlotSpec_DefaultNew( s="StateVariable", ylabel="StateVariable" )
+    StateVariableTau =   PlotSpec_DefaultNew( s="StateTimeConstant",yunit=pq.millisecond, ylabel="Time Constant"  )
+    StateVariableInf =   PlotSpec_DefaultNew( s="StateSteadyState", ylabel="Steady State" )
+    Event =              PlotSpec_DefaultNew( s="Event", ylabel="Events" )
+
+
+
+
+
+class TagViewer(object):
+
+    MPL_AUTO_SHOW = True
+
+
+    defaultPlotSpecs = (
+        DefaultPlotSpec.Voltage,
+        DefaultPlotSpec.CurrentDensity,
+        DefaultPlotSpec.Current,
+        DefaultPlotSpec.Conductance,
+        DefaultPlotSpec.ConductanceDensity,
+        DefaultPlotSpec.StateVariable,
+        DefaultPlotSpec.StateVariableTau,
+        DefaultPlotSpec.StateVariableInf,
+        DefaultPlotSpec.Event,
+        )
+
+
+
+    def __init__(self,
+                 input,
+                 fig_kwargs = {'figsize': (12, 8)},
+                 timeranges=(None,),
+                 plotspecs = None,
+                 #post_ax_functors= None,
+                 figtitle = None,
+                 show=True,
+                 save=True,
+                 linkage = None,
+                 timerange=None,
+                 additional_plotspecs = None,
+                 share_x_labels=True,
+                 mpl_tight_bounds = True
+                 ):
+
+        self.linkage = linkage
+
+
+        if timerange is not None:
+            timeranges = [timerange,]
+
+
+        if not is_iterable( input ):
+            input = [input]
+
+
+
+        # For each type of input; this should return a list of traces:
+        self.allTraceObjs = []
+        self.allEventSetObjs = []
+        trace_extractors = {
+            SimulationResult:       lambda i: self.allTraceObjs.extend( i.traces ),
+            TraceFixedDT:          lambda i: self.allTraceObjs.append( i ),
+            TraceVariableDT:       lambda i: self.allTraceObjs.append( i ),
+            TracePiecewise:        lambda i: self.allTraceObjs.append( i ),
+            EventSet:               lambda i: self.allEventSetObjs.append(i)
+                            }
+
+        for i in input:
+            tr_extractor = trace_extractors[ type(i) ]
+            tr_extractor(i)
+
+
+        # Use the new PlotSpec architecture:
+        # Filter out which plotspecs are actually going to display something,
+        # and filter out the rest:
+        plotspecs = plotspecs if plotspecs is not None else TagViewer.defaultPlotSpecs
+
+        if additional_plotspecs:
+            plotspecs = tuple( list(plotspecs) + list(additional_plotspecs) )
+
+        self.plot_specs = [ sp for sp in plotspecs if
+                            [ tr for tr in self.allTraceObjs if sp.addtrace_predicate(tr)] or  \
+                            [ evset for evset in self.allEventSetObjs if sp.addeventset_predicate(evset)] \
+                           ]
+
+
+        self.fig_kwargs = fig_kwargs
+        self.figtitle = figtitle
+        self.mpl_tight_bounds = mpl_tight_bounds
+
+        #self.post_ax_functors = post_ax_functors
+
+        self.timeranges = timeranges
+        self.share_x_labels = share_x_labels
+
+
+        self.fig = None
+        self.subaxes = []
+        self.create_figure()
+
+
+
+        # Save the figure:
+        if save:
+            PM.save_figure( figtitle )
+
+
+        if TagViewer.MPL_AUTO_SHOW and show:
+            import pylab
+            pylab.show()
+
+
+
+    def create_figure(self):
+        self.fig = QuantitiesFigure(**self.fig_kwargs)
+
+
+        # Add a title to the plot:
+        if self.figtitle:
+            self.fig.suptitle(self.figtitle)
+
+
+        # Work out what traces are on what graphs:
+        ps_to_traces = dict([ (ps,[tr for tr in self.allTraceObjs if ps.addtrace_predicate(tr) ]) for ps in self.plot_specs  ])
+        if self.linkage:
+            self.linkage.process(ps_to_traces)
+
+
+        n_time_ranges = len(self.timeranges)
+        n_plots = len(self.plot_specs)
+
+
+        #time_axis = None
+
+        for i, plot_spec in enumerate( self.plot_specs ):
+
+            print 'Plotting For PlotSpec:', plot_spec
+
+            for iT, time_range in enumerate(self.timeranges):
+
+
+                time_range = _resolve_time_range(time_range)
+
+                # Create the axis:
+                ax = self.fig.add_subplot(n_plots, n_time_ranges, i*n_time_ranges + iT  + 1 )
+                ax.set_xunit(pq.millisecond)
+
+                # Leave the plotting to the PlotSpecification
+                is_bottom_plot = i==n_plots-1
+                plot_xaxis_details = is_bottom_plot or not self.share_x_labels
+                plot_spec.plot( ax=ax, all_traces=self.allTraceObjs, all_eventsets=self.allEventSetObjs, time_range=time_range, linkage=self.linkage, plot_xaxis_details=plot_xaxis_details )
+
+                # Save the Axis:
+                self.subaxes.append(ax)
+
+
+        if self.mpl_tight_bounds:
+            import pylab
+            pylab.tight_layout()
+
+
+
