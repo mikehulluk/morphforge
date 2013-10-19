6ecd0886817f675fea5c159d3d26607aac1e018d
Michael Hull <mikehulluk@googlemail.com>
2013-10-19 18:24:59 +0100

    ...

--- doc/conf.py
+++ doc/conf.py
@@ -49,6 +49,16 @@ class Mock(object):
         else:
             return Mock()
 
+    def __getattr__(cls, name):
+        if name in ('__file__', '__path__'):
+            return '/dev/null'
+        elif name[0] == name[0].upper():
+            mockType = type(name, (), {})
+            mockType.__module__ = __name__
+            return mockType
+        else:
+            return Mock()
+
 MOCK_MODULES = ['numpy', 'pylab', 'scipy', 'mredoc', 'mreorg']
 for mod_name in MOCK_MODULES:
     sys.modules[mod_name] = Mock
