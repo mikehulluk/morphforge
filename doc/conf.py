5e4c1f65cb6b78d0d5d6ca322ab1aaebf8b1f665
Michael Hull <mikehulluk@googlemail.com>
2013-10-19 18:22:21 +0100

    Building on RTD

--- doc/conf.py
+++ doc/conf.py
@@ -27,6 +27,39 @@ import os
 
 
 
+
+# To allow stuff to build on RTD
+import sys
+
+class Mock(object):
+    def __init__(self, *args, **kwargs):
+        pass
+
+    def __call__(self, *args, **kwargs):
+        return Mock()
+
+    @classmethod
+    def __getattr__(cls, name):
+        if name in ('__file__', '__path__'):
+            return '/dev/null'
+        elif name[0] == name[0].upper():
+            mockType = type(name, (), {})
+            mockType.__module__ = __name__
+            return mockType
+        else:
+            return Mock()
+
+MOCK_MODULES = ['numpy', 'pylab', 'scipy', 'mredoc', 'mreorg']
+for mod_name in MOCK_MODULES:
+    sys.modules[mod_name] = Mock
+
+
+
+
+
+
+
+
 sys.path.append('../src/')
 
 
