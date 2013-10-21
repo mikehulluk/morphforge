381499209d40d59bb55ed5c831ba51f8d2802caf
Michael Hull <mikehulluk@googlemail.com>
2013-10-21 20:15:21 +0100

    ...

--- src/morphforgeexamples/exset1_morphology/morphology050.py
+++ src/morphforgeexamples/exset1_morphology/morphology050.py
@@ -43,20 +43,20 @@ m = mf.MorphologyTree.fromMorphML(src=open(srcMorphMLFile) )
 
 
 
-# TODO - SPEAK TO PADRAIG:
-raise NotImplementedError()
-srcMorphMLFile = mf.Join(testSrcsPath, "neuroml/morphml/L23PyrFRB.morph.xml",)
-m = mf.MorphologyTree.fromMorphML(
-        src=open(srcMorphMLFile),
-        regions={
-            'all':'Rgn1',
-            'ModelViewParmSubset_1':'Rgn2',
-            'ModelViewParmSubset_3':'Rgn2',
-            'ModelViewParmSubset_8':'Rgn2',
-            'OneSecGrp_SectionRef_1':'Rgn1',
-        }
-)
-mf.MayaViRenderer(m).show_as_points_interpolated()
+## TODO - SPEAK TO PADRAIG:
+#raise NotImplementedError()
+#srcMorphMLFile = mf.Join(testSrcsPath, "neuroml/morphml/L23PyrFRB.morph.xml",)
+#m = mf.MorphologyTree.fromMorphML(
+#        src=open(srcMorphMLFile),
+#        regions={
+#            'all':'Rgn1',
+#            'ModelViewParmSubset_1':'Rgn2',
+#            'ModelViewParmSubset_3':'Rgn2',
+#            'ModelViewParmSubset_8':'Rgn2',
+#            'OneSecGrp_SectionRef_1':'Rgn1',
+#        }
+#)
+#mf.MayaViRenderer(m).show_as_points_interpolated()
 
 
 
