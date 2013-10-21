a291b320ea0b7893dc17df608827e5720378b3da
Michael Hull <mikehulluk@googlemail.com>
2013-10-21 16:31:32 +0100

    building docs for rtd

--- doc/conf.py
+++ doc/conf.py
@@ -50,7 +50,7 @@ class Mock(object):
             return Mock()
 
 
-MOCK_MODULES = ['numpy', 'pylab', 'scipy', 'mredoc', 'mreorg', 'neurounits' ]
+MOCK_MODULES = ['numpy', 'pylab', 'scipy', 'mredoc', 'mreorg', 'neurounits', 'quantities']
 for mod_name in MOCK_MODULES:
     sys.modules[mod_name] = Mock
 
